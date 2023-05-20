e78a0523e75c79d901c16c896ffdfdf0248267e3
sagiv bonali <sagivb13@gmail.com>
2023-05-20 11:29:16 +0300

    Show CreatePollForm in home page

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
-  import Tabs from './shared/Tabs.svelte';
-  let items = ['Current Polls', 'Add New Poll'];
-  let activeItem = 'Current Polls';
-  const tabChange = (e) => activeItem = e.detail;
+  import CreatePollForm from "./components/CreatePollForm.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+  const tabChange = (/** @type {{ detail: string; }} */ e) =>
+    (activeItem = e.detail);
 </script>
 
-
 <Tabs {activeItem} {items} on:tabChange={tabChange} />
+{#if activeItem === "Add New Poll"}
+  <CreatePollForm />
+{/if}
