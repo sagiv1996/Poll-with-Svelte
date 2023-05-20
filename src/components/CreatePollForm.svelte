0cde956ec286c937944688e98a880c62930c1e3c
sagiv bonali <sagivb13@gmail.com>
2023-05-20 13:15:23 +0300

    Add button to delete value

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -43,18 +43,50 @@
     answers = [...answers, ""];
     errors.answers = [...errors.answers, ""];
   };
+
+  function handleDelete(/** @type {Number} **/ index) {
+    if (answers.length <= 1) return;
+    answers = answers.filter((answer, ind) => ind !== index);
+    errors.answers = errors.answers.filter((answer, ind) => ind !== index);
+  }
 </script>
 
+<head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    integrity="sha512-*******************"
+    crossorigin="anonymous"
+  />
+</head>
 <form on:submit|preventDefault={submitHandler}>
   <div class="form-field">
     <label for="question">Poll Question:</label>
-    <input type="text" id="question" bind:value={question} />
+    <input type="text" class="question" id="question" bind:value={question} />
     <div class="error">{errors.question}</div>
   </div>
   {#each answers as answer, index}
     <div class="form-field">
-      <label for="answer-{index}">Answer {index + 1} value:</label>
-      <input type="text" id="answer-{index}" bind:value={answers[index]} />
+      <label class="answer" for="answer-{index}"
+        >Answer {index + 1} value:</label
+      >
+
+      {#if answers.length > 1}
+        <button
+          on:click={() => handleDelete(index)}
+          type="button"
+          class="delete-button"
+        >
+          <i class="fas fa-trash-alt" />
+        </button>
+      {/if}
+      <input
+        class="answer"
+        type="text"
+        id="answer-{index}"
+        bind:value={answers[index]}
+      />
+
       <div class="error">{errors.answers[index]}</div>
     </div>
   {/each}
@@ -72,8 +104,15 @@
     margin: 18px auto;
   }
   input {
-    width: 100%;
     border-radius: 6px;
+  }
+  input.question,
+  label.answer {
+    width: 100%;
+  }
+
+  input.answer {
+    width: 50%;
   }
   label {
     margin: 10px auto;
@@ -83,5 +122,10 @@
     font-weight: bold;
     font-size: 12px;
     color: #d91b42;
+  }
+  .delete-button {
+    background: none;
+    border: none;
+    cursor: pointer;
   }
 </style>
