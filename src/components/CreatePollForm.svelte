6b99f47e9f2cc4a7f9a26fcd34a551e1fa66161c
sagiv bonali <sagivb13@gmail.com>
2023-05-20 11:28:57 +0300

    Create CreatePollFrom component

---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,78 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import Button from "../shared/Button.svelte";
+
+  let question = "";
+  let errors = { question: "", answers: [] };
+  let answers = [];
+  let valid = false;
+
+  const resetErrorsFrom = () => {
+    errors = { question: "", answers: [] };
+  };
+
+  const submitHandler = () => {
+    valid = true;
+    resetErrorsFrom();
+    if (question.trim().length < 5) {
+      valid = false;
+      errors.question = "Question must be at least 5 chars long";
+    }
+
+    answers.forEach((answer, index) => {
+      errors.answers[index] =
+        answer.trim().length < 3
+          ? "Question must be at least 3 chars long"
+          : "";
+    });
+    if (valid) {
+      // Add Poll for global other polls
+    }
+  };
+
+  const addAnswerHandler = () => {
+    answers = [...answers, ""];
+    errors.answers = [...errors.answers, ""];
+  };
+</script>
+
+<form on:submit|preventDefault={submitHandler}>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question" bind:value={question} />
+    <div class="error">{errors.question}</div>
+  </div>
+  {#each answers as answer, index}
+    <div class="form-field">
+      <label for="answer-{index}">Answer {index + 1} value:</label>
+      <input type="text" id="answer-{index}" bind:value={answers[index]} />
+      <div class="error">{errors.answers[index]}</div>
+    </div>
+  {/each}
+  <Button type="button" on:click={addAnswerHandler}>Add answer</Button>
+  <Button>Add Poll</Button>
+</form>
+
+<style>
+  form {
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .form-field {
+    margin: 18px auto;
+  }
+  input {
+    width: 100%;
+    border-radius: 6px;
+  }
+  label {
+    margin: 10px auto;
+    text-align: left;
+  }
+  .error {
+    font-weight: bold;
+    font-size: 12px;
+    color: #d91b42;
+  }
+</style>
